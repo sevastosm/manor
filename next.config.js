c14fad607d251c35294ca579a46bb8d9aeb77a21
Sevastos Matzouranis <Sevastos.Matzouranis@arhs-dev-hellas.com>
2023-07-05 19:08:58 +0300

    changes next settings

--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
 /** @type {import('next').NextConfig} */
 
 const repo = "manor";
-const assetPrefix = `/${repo}/`;
+const assetPrefix = `/${repo}`;
 const basePath = `/${repo}`;
 
 const nextConfig = {
   reactStrictMode: true,
   domains: ["localhost", "sevastosm.github.io", "fonts.googleapis.com"],
   images: {
-    domains: ["sevastosm.github.io", "localhost", "media.graphassets.com"],
+    // domains: ["sevastosm.github.io", "localhost", "media.graphassets.com"],
     unoptimized: true,
-    path: "./",
+    // path: "./",
   },
   assetPrefix: assetPrefix,
   basePath: basePath,
